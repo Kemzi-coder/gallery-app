24075048f7e7430cfbdff1b71465aac66d404fc6
Max Kemzi <kemzi9585@gmail.com>
2022-08-18 17:21:51 +0300

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: ["./tsconfig.json"]
+	},
+	settings: {
+		react: {
+			version: "detect"
+		}
+	},
+	extends: [
+		"airbnb",
+		"airbnb-typescript",
+		"airbnb/hooks",
+		"plugin:@typescript-eslint/recommended",
+		"prettier"
+	],
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"prettier/prettier": ["error", {endOfLine: "auto"}],
+		"react/prop-types": "off",
+		"react/function-component-definition": [
+			"error",
+			{
+				namedComponents: "arrow-function"
+			}
+		],
+		"no-console": "off",
+		"react/jsx-props-no-spreading": "off",
+		"react/react-in-jsx-scope": "off",
+		"jsx-a11y/label-has-associated-control": [
+			2,
+			{
+				assert: "either"
+			}
+		],
+		"react/require-default-props": "off",
+		"no-param-reassign": "off",
+		"react/jsx-no-useless-fragment": ["error", {allowExpressions: true}],
+		"global-require": "off",
+		"@typescript-eslint/default-param-last": "off"
+	}
+};
