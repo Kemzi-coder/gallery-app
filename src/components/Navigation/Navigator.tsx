53e252ac41e6b1b10f8222279a376b6691fc2e68
Max Kemzi <kemzi9585@gmail.com>
2022-08-18 17:25:05 +0300

    Add navigator component Needed to split code

---
+++ src/components/Navigation/Navigator.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Colors from "../../utils/constants/colors";
+import ScreenNames from "../../utils/constants/screenNames";
+import PhotosScreen from "../../screens/Photos/PhotosScreen/PhotosScreen";
+import PhotoScreen from "../../screens/Photo/PhotoScreen/PhotoScreen";
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => (
+	<Stack.Navigator
+		screenOptions={{
+			headerStyle: {
+				backgroundColor: Colors.Lighter
+			},
+			headerShadowVisible: false,
+			headerTitleStyle: {
+				fontFamily: "OpenSans-Bold",
+				fontSize: 32,
+				color: Colors.Primary
+			}
+		}}
+		initialRouteName={ScreenNames.Photos}
+	>
+		<Stack.Screen name={ScreenNames.Photos} component={PhotosScreen} />
+		<Stack.Screen name={ScreenNames.Photo} component={PhotoScreen} />
+	</Stack.Navigator>
+);
+
+export default Navigator;
