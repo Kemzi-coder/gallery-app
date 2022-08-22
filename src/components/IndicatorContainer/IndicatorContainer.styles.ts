82d19ee63bf5ab5c65af6297596ad09685749972
Max Kemzi <kemzi9585@gmail.com>
2022-08-22 14:25:09 +0300

    Change indicator container styles

--- src/components/IndicatorContainer/IndicatorContainer.styles.ts
+++ src/components/IndicatorContainer/IndicatorContainer.styles.ts
@@ -1,7 +1,18 @@
 import {StyleSheet} from "react-native";
+import Colors from "../../utils/constants/colors";
 
 const styles = StyleSheet.create({
-	container: {flex: 1, justifyContent: "center", alignItems: "center"}
+	container: {
+		position: "absolute",
+		left: 0,
+		right: 0,
+		top: 0,
+		bottom: 0,
+		opacity: 0.4,
+		backgroundColor: Colors.Primary,
+		justifyContent: "center",
+		alignItems: "center"
+	}
 });
 
 export default styles;
